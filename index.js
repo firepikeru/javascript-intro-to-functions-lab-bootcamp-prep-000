f6004f2729049dd8f0486a3bc125216e6980b893
yingmei09@gmail.com <yingmei09@gmail.com>
2018-09-09 19:54:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,11 +18,19 @@ function sayHiToGrandma(string) {
   if (string == string.toLowerCase()){
     return("I can\'t hear you!");
   }
+<<<<<<< HEAD
   else if (string == string.toUpperCase()){
     return("YES INDEED!");
   }
   else {
     return("I love you, too.");
+=======
+  else if (string ==string.toUpperCase()){
+    console.log("YES, INDEED!");
+  }
+  else {
+  console.log("I love you, too.");
+>>>>>>> 3e08ff984dcac629127646c9b47d7a805d98cc78
   }
 }
   
